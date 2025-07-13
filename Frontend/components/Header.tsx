47d442b2c778bdde8279a4d1a7dad86dd467d7d6
Aditi Agrawal <118436069+aditi-agrawal12@users.noreply.github.com>
2025-07-13 14:15:45 +0530

    Update Header.tsx

--- Frontend/components/Header.tsx
+++ Frontend/components/Header.tsx
@@ -72,7 +72,7 @@ export const Header = () => {
 
   return (
     <header className="sticky top-0 z-10 flex items-center justify-between gap-4 sm:gap-6 min-h-20 px-4 md:px-6 border-b border-border bg-background text-foreground shadow-md backdrop-blur-md">
-      <ThemeToggle/>
+     
       {pathname?.startsWith("/profile") || pathname === "/rewardsstore" ? (
         <Link href="/dashboard" className="flex items-center min-w-[120px]">
           <Image
